8370dc1cfe4a347be8e13e00cb5c66e2bd004742
Alex8320 <alex.heredia.barbosa@gmail.com>
2017-01-15 13:12:31 -0500

    Adding folder for apk

--- javascript/score.js
+++
@@ -1,5 +0,0 @@
-function updateScore(){
-	var score_element = document.getElementById("tspan17169");
-	var score = Number(score_element.innerHTML);
-	score_element.innerHTML = score+1;
-}
--- layers/README.md
+++
@@ -1,2 +0,0 @@
-These files are used by flappy2.svg
-If you like to work on them, please commen here: https://github.com/fossasia/flappy-svg/issues/108
